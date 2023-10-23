cb3c4f07e19326ebad8f4460df939f6635d4d46c
Le Nguyen <le.nguyenthimy@asnet.com.vn>
2023-10-23 11:33:24 +0700

    init app

---
+++ src/app.py
@@ -0,0 +1,18 @@
+import web
+
+urls = (
+  '/', 'index'
+)
+
+# app = web.application(urls, globals())
+
+
+class index:
+    def GET(self):
+        greeting = "Hello World"
+        return greeting
+
+
+if __name__ == '__main__' :
+    app = web.application(urls, globals())
+    app.run()
